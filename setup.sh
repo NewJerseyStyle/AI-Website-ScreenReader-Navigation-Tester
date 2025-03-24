3e4c0adcbd0ba0d367faa25acc3d30d4bf32c2d0
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-03-24 14:43:04 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ npm install @guidepup/playwright @playwright/test
 npm install langchain @langchain/core @langchain/groq zod
 # Install WebKit browser for Playwright.
 npx playwright install webkit
-# Or you can let Playwright pick its favorite browser.
-npx playwright install
+# # Or you can let Playwright pick its favorite browser.
+# npx playwright install
+npx @guidepup/setup
